07bc20472dcea98e7b98070a1833e73ea8bbd883
Karolina Szymanska <karolinaszymanska899@gmail.com>
2021-10-20 22:10:53 +0200

    added the beginning of the function

--- js/script.js
+++ js/script.js
@@ -122,3 +122,8 @@ whatIDontLike(null);
 // and assign the return value to be the innerHTML value of the element with the id subtraction.
 // Test your function with the following value pairs: (1, 42), (200, 150),
 // (10, "50"), ("100", "400"), ("Ten", "One Hundred"), (null, 123);
+
+function subtraction(number1, number2) {
+  var convertedNumber1 = parseFloat(number1);
+  var convertedNumber2 = parseFloat(number2);
+}
