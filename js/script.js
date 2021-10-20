943f8eca6f0bc69412f2029662c2cb61b5782a05
Karolina Szymanska <karolinaszymanska899@gmail.com>
2021-10-20 22:07:04 +0200

    pasted the instruction for question 5.

--- js/script.js
+++ js/script.js
@@ -107,3 +107,18 @@ whatIDontLike("brussels sprout");
 whatIDontLike(25);
 whatIDontLike(true);
 whatIDontLike(null);
+
+// question 5
+// Create a function that accepts two arguments.
+// Inside the function, try to convert the arguments to numbers.
+
+// If either of the arguments cannot be converted to a number,
+// return the value: "Invalid argument(s)".
+
+// If both arguments are number values or can be converted to number values,
+// subtract the second argument from the first and return the result from the function.
+
+// Call the function, pass in to two values
+// and assign the return value to be the innerHTML value of the element with the id subtraction.
+// Test your function with the following value pairs: (1, 42), (200, 150),
+// (10, "50"), ("100", "400"), ("Ten", "One Hundred"), (null, 123);
