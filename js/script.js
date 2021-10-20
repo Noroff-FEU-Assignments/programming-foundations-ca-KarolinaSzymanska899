53f26b509c5bcf72d8351cc9ee145d4709b3aa72
Karolina Szymanska <karolinaszymanska899@gmail.com>
2021-10-20 10:44:09 +0200

    pasted instruction for question 1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// question 1 CORRECT
+// You are creating an ecommerce application selling products online.
+// Some of the products are no longer in stock, and the user must be made aware of this.
+// Create a variable called outOfStock and assign it a boolean value.
+// Create an if else statement that checks the value of outOfStock.
+// If it is true, console log "Out of stock". Otherwise log "In stock".
