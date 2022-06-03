2d0dc20249520c1beacdafd3b8557ec15a99fe99
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-06-03 22:05:32 +0200

    many corrections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 // question 1
 
 var outOfStock = false;
@@ -17,6 +18,9 @@ for (var i = 15; i <= 25; i++) {
 }
 
 // question 3
+// You need to do an explicit "null" check here as well.
+// "null <= 3.5" evaluates to true, so you will be printing out The title
+// and null.
 
 var games = [
   {
@@ -148,3 +152,81 @@ buttonCalculatePrice.onclick = function calculatePrice() {
   var total = document.querySelector("#total");
   total.innerHTML = theTotal;
 };
+*/
+
+// question 5:
+
+// Create a function that accepts two arguments.
+// Inside the function, try to convert the arguments to numbers.
+
+// If either of the arguments cannot be converted to a number,
+// return the value: "Invalid argument(s)".
+
+// If both arguments are number values or can be converted to number values,
+// subtract the second argument from the first and return the result from the function.
+
+// Call the function, pass in to two values
+// and assign the return value to be the innerHTML value of the element with the id subtraction.
+// Test your function with the following value pairs: (1, 42), (200, 150),
+// (10, "50"), ("100", "400"), ("Ten", "One Hundred"), (null, 123);
+
+function subtraction(number1, number2) {
+  const convertedNumber1 = parseFloat(number1);
+  const convertedNumber2 = parseFloat(number2);
+  const subtractionContainer = document.querySelector("#subtraction");
+
+  if (isNaN(convertedNumber1) || isNaN(convertedNumber2)) {
+    subtractionContainer.innerHTML += "Invalid argument(s)";
+    console.log("Invalid argument(s)");
+  } else {
+    const result = convertedNumber1 - convertedNumber2;
+    subtractionContainer.innerHTML = result;
+    return result;
+  }
+}
+
+subtraction(100, "100");
+
+// question 6
+// Select the button with the class page.
+// When the button is clicked do the following:
+// Change the title of the page (the value of the title element in the head) to "Updated title".
+// Change the background colour of the whole page to "yellow".
+// Change the color of the h1 element to "green".
+// Change the h1 element's font to "impact".
+// Change the value of the h1 element to be a link.
+// Remove the list style and padding from the ul element.
+// The h1 element should look like this after the click event:
+// <h1><a href="#">Programming Foundations Course Assignment</a></h1>
+
+const buttonPage = document.querySelector(".page");
+const heading = document.querySelector("h1");
+const list = document.querySelector("ul");
+
+buttonPage.onclick = function () {
+  document.title = "Updated title";
+};
+
+// question 7
+// Select the button with the class price.
+// When the button is clicked, loop through the array below, add all the prices
+// and assign the total to be the innerHTML value of the element with the id total:
+
+var toys = [
+  {
+    name: "Lego",
+    price: 15.6,
+  },
+  {
+    name: "Master of the Universe",
+    price: "28.3",
+  },
+  {
+    name: "Barbie",
+    price: null,
+  },
+  {
+    name: "Mr Potato Head",
+    price: 89.99,
+  },
+];
