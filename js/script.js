667298dffaf028f89fba8a7d81cdf53c235e7631
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-06-07 16:06:53 +0200

    corrected question 5.

--- js/script.js
+++ js/script.js
@@ -119,6 +119,19 @@ whatIDontLike(null);
 */
 
 // question 5
+// Create a function that accepts two arguments.
+// Inside the function, try to convert the arguments to numbers.
+
+// If either of the arguments cannot be converted to a number,
+// return the value: "Invalid argument(s)".
+
+// If both arguments are number values or can be converted to number values,
+// subtract the second argument from the first and return the result from the function.
+
+// Call the function, pass in to two values
+// and assign the return value to be the innerHTML value of the element with the id subtraction.
+// Test your function with the following value pairs: (1, 42), (200, 150),
+// (10, "50"), ("100", "400"), ("Ten", "One Hundred"), (null, 123);
 
 function subtraction(number1, number2) {
   const convertedNumber1 = parseFloat(number1);
@@ -127,6 +140,7 @@ function subtraction(number1, number2) {
 
   if (isNaN(convertedNumber1) || isNaN(convertedNumber2)) {
     subtractionContainer.innerHTML += "Invalid argument(s)";
+    console.log("Invalid argument(s)");
   } else {
     const result = convertedNumber1 - convertedNumber2;
     subtractionContainer.innerHTML = result;
@@ -134,7 +148,7 @@ function subtraction(number1, number2) {
   }
 }
 
-subtraction(100, "90");
+subtraction(100, "100");
 
 /*
 // question 6
