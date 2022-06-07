b29dd367d4409d2c43d9a581f56ff9eb38171f87
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-06-07 15:26:49 +0200

    added the instruction in the question 2.

--- js/script.js
+++ js/script.js
@@ -13,8 +13,10 @@ if (outOfStock) {
   console.log("In stock");
 }
 
-/*
 // question 2
+// Create a for loop that counts from 15 to 25.
+// Console log the value of the counter variable inside the loop only if it's
+// equal to 17 or 20.
 
 for (var i = 15; i <= 25; i++) {
   if (i === 17 || i === 20) {
@@ -22,6 +24,7 @@ for (var i = 15; i <= 25; i++) {
   }
 }
 
+/*
 // question 3
 
 var games = [
